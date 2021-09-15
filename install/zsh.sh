6e01d0689e47bc007fc44cc15c023e4a26dac9c3
kyunggeun-lee <kyunggeun@kyunggeu.ap.qualcomm.com>
2021-09-15 17:31:36 +0900

    Install zsh locally

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # zsh
-sudo apt install -y zsh
-chsh -s $(which zsh)
+VERSION=${VERSION:-5.8}
+rm -rf $HOME/.local/bin/zsh $HOME/.local/bin/zsh-*.* $HOME/.local/lib/zsh $HOME/.local/share/zsh
+cd /tmp
+wget https://sourceforge.net/projects/zsh/files/zsh/$VERSION/zsh-$VERSION.tar.xz
+tar -xvf zsh-$VERSION.tar.xz
+cd zsh-$VERSION
+mkdir -p $HOME/.local
+./configure --prefix=$HOME/.local
+make && make install
 
 # oh-my-zsh
 cd /tmp
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+source $HOME/.zshrc
 
 # zsh-autosuggestions
-zsh -c "git clone https://github.com/zsh-users/zsh-autosuggestions $(echo $ZSH_CUSTOM)/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
