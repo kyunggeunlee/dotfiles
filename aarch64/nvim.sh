913af9aa39517cf9620f48eca8c5178512d12804
Kyunggeun Lee <kyunggeunlee@github.com>
2025-10-14 12:32:17 -0700

    tmp

---
+++ aarch64/nvim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ev
+
+pushd /tmp
+
+# Get nightly release
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-arm64.tar.gz
+tar xzf nvim-linux-arm64.tar.gz
+mkdir -p ~/.local
+
+# Remove
+rm -f ~/.local/bin/nvim    || true
+rm -rf ~/.local/lib/nvim   || true
+rm -rf ~/.local/share/nvim || true
+
+# Install nvim
+rsync -rltoD nvim-linux-arm64/* ~/.local/
+
+# Install plugins
+sh -c 'curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+$HOME/.local/bin/nvim -E -s -u ~/.config/nvim/init.vim +PlugInstall +qall!
+
+popd
--- x86/pyright.sh
+++ x86/pyright.sh
@@ -4,10 +4,10 @@ set -ev
 pushd /tmp
 
 # nodejs
-NODE_VERSION=${VERSION:-14.17.6}
-curl -LO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz"
+NODE_VERSION=${VERSION:-22.14.0}
+curl -LO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-arm64.tar.xz"
 
-tar -xf node-v$NODE_VERSION-linux-x64.tar.xz
+tar -xf node-v$NODE_VERSION-linux-arm64.tar.xz
 
 # Create directory
 mkdir -p $HOME/.local/src
@@ -15,7 +15,7 @@ mkdir -p $HOME/.local/src
 # Remove
 rm -rf $HOME/.local/node || true
 
-mv node-v$NODE_VERSION-linux-x64 $HOME/.local/node
+mv node-v$NODE_VERSION-linux-arm64 $HOME/.local/node
 
 # pyright
 PATH=$HOME/.local/node/bin:$PATH $HOME/.local/node/bin/npm install -g pyright
