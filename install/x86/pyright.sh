a3d9e0a03c8e77e690af2910d39e7221369f9a76
Kyunggeun Lee <kyunggeunlee@github.com>
2025-10-14 12:34:52 -0700

    Add aarch64 install scripts

---
+++ install/x86/pyright.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ev
+
+pushd /tmp
+
+# nodejs
+NODE_VERSION=${VERSION:-22.14.0}
+curl -LO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz"
+
+tar -xf node-v$NODE_VERSION-linux-x64.tar.xz
+
+# Create directory
+mkdir -p $HOME/.local/src
+
+# Remove
+rm -rf $HOME/.local/node || true
+
+mv node-v$NODE_VERSION-linux-x64 $HOME/.local/node
+
+# pyright
+PATH=$HOME/.local/node/bin:$PATH $HOME/.local/node/bin/npm install -g pyright
+
+popd
